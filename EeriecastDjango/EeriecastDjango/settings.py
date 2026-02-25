479ea90ae6341b67932a4cda8800b40a539e439d
Jacob Durante <jacob.durante@bitbenders.com>
2026-02-25 18:12:12 -0500

    more tweaks to back-end settings

--- EeriecastDjango/EeriecastDjango/settings.py
+++ EeriecastDjango/EeriecastDjango/settings.py
@@ -110,11 +110,10 @@ if DEBUG:
 else:
     if not DJANGO_CORS_OVERRIDE:
         CORS_ORIGIN_WHITELIST = [
-            # TODO: add to whitelist
+            "https://eeriecast-app-test.vercel.app/"
         ]
     else:
         CORS_ORIGIN_ALLOW_ALL = True
-
 
 # Application definition
 
@@ -163,8 +162,16 @@ EMAIL_BACKEND = config(
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='brenden@eeriecast.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='djkhisqktjmxxzbm')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 465  # Changed to integer
+# EMAIL_HOST_USER = 'info@pocketchef.io'
+# EMAIL_HOST_PASSWORD = 'notzfccwltxziseu'
+# EMAIL_USE_SSL = True  # Changed from TLS to SSL
+# EMAIL_USE_TLS = False  # Explicitly disable TLS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
