f438d7694cc92d58b132987e4e1cce743fa6aeb9
Jacob Durante <jacob.durante@bitbenders.com>
2026-04-20 02:20:33 -0400

    small tweak

--- EeriecastReact/src/constants.js
+++ EeriecastReact/src/constants.js
@@ -1,4 +1,4 @@
-//export const API_BASE_URL = 'https://backend.eerie.fm/api';
-export const API_BASE_URL = 'http://localhost:8012/api';
+export const API_BASE_URL = 'https://backend.eerie.fm/api';
+//export const API_BASE_URL = 'http://localhost:8012/api';
 
 export const STRIPE_PUBLIC_KEY = 'pk_test_51NgsNSDZQqr6bWzeMdhzAHgTAEntrvf1CYPyXTuuiUwJg5Tw23MzLNTN2lnvsExdCgwLNdFUGIxB4GDUsWTP8mHP00IQ9YjptB';
